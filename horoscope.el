d6b23968bbb2e2926a9ac656af28fcb7a928783c
mschuldt <mbschuldt@gmail.com>
2018-03-27 23:49:38 -0700

    commentary

--- horoscope.el
+++ horoscope.el
@@ -28,6 +28,10 @@
 
 ;;; Commentary:
 
+;; Emacs is your astrologer, read its interpretation with:
+;;   M-x horoscope
+;; No need to tell it your birth date, Emacs already knows you well enough.
+;;
 ;; Bob Manson originally wrote this program in ksh,
 ;; then later converted it to Emacs Lisp.
 ;; Noah Friedman rewrote parts of the engine.
