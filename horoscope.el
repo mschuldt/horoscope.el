b0a456abd70c73ba20620a16c5d2081086013bea
mschuldt <mbschuldt@gmail.com>
2018-04-03 19:17:50 -0700

    license

--- horoscope.el
+++ horoscope.el
@@ -12,6 +12,19 @@
 ;; If this is useful to you, may you forever be blessed by the Holy Lord
 ;; Patty.  AT&T you will.
 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; Bob Manson originally wrote this program in ksh,
